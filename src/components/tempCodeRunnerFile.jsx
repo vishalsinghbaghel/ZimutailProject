d62f3bfc40d1056acce18e9d590d20ac6bb4d586
vishalsinghbaghel <vishalsinghbaghel828@gmail.com>
2025-09-12 18:56:46 +0530

    change

---
+++ src/components/tempCodeRunnerFile.jsx
@@ -0,0 +1,7 @@
+<div className={styles.icon}>
+                <img
+                  src={feature.icon}
+                  alt={feature.title}
+                  className={styles.iconImg}
+                />
+              </div>

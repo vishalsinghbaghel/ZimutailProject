0c2c862fda5096ea830479c9fadea533185b6624
vishalsinghbaghel <vishalsinghbaghel828@gmail.com>
2025-09-13 15:17:15 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-// src/App.jsx
+
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import styles from "./App.module.css";
-import { Navbar } from "./components/navbar.jsx";
+import { Navbar } from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import { Features } from "./components/Features.jsx";
 import HowItWorks from "./components/HowItWorks.jsx";
 import WhyChooseUs from "./components/WhyChooseUs.jsx";
 import {Footer} from "./components/footer.jsx";
-// import Home from "./pages/Home.jsx";
-// import About from "./pages/About.jsx";
-// import Contact from "./pages/Contact.jsx";
-// import Seller from "./pages/Seller.jsx";
+
 
 export default function App() {
   return (
@@ -22,14 +19,7 @@ export default function App() {
       <HowItWorks />
       <WhyChooseUs />
       <Footer/>
-      <main>
-        {/* <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/seller" element={<Seller />} />
-        </Routes> */}
-      </main>
+      
     </div>
   );
 }
